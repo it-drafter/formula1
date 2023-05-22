2976e4a900e26ddb401f09f9da497e3d8daa2acf
ivanvk63 <ivanvk@gmail.com>
2023-05-22 13:43:17 +0200

    dodao dve komponenete detalja

---
+++ src/components/RaceQualifyingResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RaceQualifyingResults = () => {
+    return (
+        <>
+            <h1>Races Qualifying Results component</h1>
+        </>
+    );
+};
+
+export default RaceQualifyingResults;
