fb02c9d49d683170fa1cef3e4fc6fa0b9c3eda59
portal222 <dvaportala@gmail.com>
2023-05-22 15:35:06 +0200

    dodati fajl teams teamsTableRow

---
+++ src/components/TeamsTableRow.js
@@ -0,0 +1,17 @@
+import React from "react";  
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';    
+
+const TeamsTableRow = (props) => {
+
+return (
+    <TableRow>
+    <TableCell>{props.teams.position}</TableCell>
+    <TableCell>{props.teams.points}</TableCell>
+    <TableCell>{props.teams.wins}</TableCell>
+    <TableCell>{props.teams.constructor}</TableCell>
+  </TableRow>
+)
+
+}
+export default TeamsTableRow;
