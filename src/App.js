76d8fd93fd5fe7acd526d602ba4d1952cd3f0d92
itancik <itancik@gmail.com>
2023-05-22 12:29:10 +0200

    minor change in app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const App = () => {
-    return(
-        <>
-            <h1>Formula 1</h1>
-        </>
-    );
-}
+  return (
+    <>
+      <h1>Formula 1</h1>
+    </>
+  );
+};
 
 export default App;
