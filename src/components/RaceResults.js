342793dd69c3a41ff35bae19695c4a723564c695
itancik <itancik@gmail.com>
2023-05-23 12:39:09 +0200

    feat: times fixed

--- src/components/RaceResults.js
+++ src/components/RaceResults.js
@@ -1,83 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { RiseLoader } from 'react-spinners';
 import {
-    Table,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableCell,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
 } from '@mui/material';
 import axios from 'axios';
 import ResultsTableRow from './ResultsTableRow';
 
-
 const RaceResults = () => {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        getResults();
-    }, []);
+  useEffect(() => {
+    getResults();
+  }, []);
 
-    const getResults = async () => {
-        const url = 'http://ergast.com/api/f1/2013/1/results.json';
-        setIsLoading(true);
-        try {
-            const response = await axios.get(url);
-            console.log('response', response);
-            //   if (response.request.status !== 200) {
-            //     throw new Error('Something went wrong!');
-            //   }
-            const data =
-                response.data.MRData.RaceTable.Races[0].Results;
-            console.log("Time", data[0].Time.time);
-            setResults(data);
-            setIsLoading(false);
-        } catch (err) {
-            //   console.log(err);
-            setError(err);
-        }
-    };
-
-    if (error) {
-        return <p>Error: {error.message}</p>;
+  const getResults = async () => {
+    const url = 'http://ergast.com/api/f1/2013/1/results.json';
+    setIsLoading(true);
+    try {
+      const response = await axios.get(url);
+      console.log('response', response);
+      //   if (response.request.status !== 200) {
+      //     throw new Error('Something went wrong!');
+      //   }
+      const data = response.data.MRData.RaceTable.Races[0].Results;
+      //   console.log('Time', data[0].Time.time);
+      setResults(data);
+      setIsLoading(false);
+    } catch (err) {
+      //   console.log(err);
+      setError(err);
     }
+  };
 
-    if (isLoading) {
-        return (
-            <RiseLoader
-                style={{
-                    height: '50vh',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            />
-        );
-    }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
+  if (isLoading) {
     return (
-        <>
-            <h1>Results component</h1>
-            <Table>
-                <TableHead>
-                    <TableRow className='table-header'>
-                        <TableCell>Pos</TableCell>
-                        <TableCell>Driver</TableCell>
-                        <TableCell>Team</TableCell>
-                        <TableCell>Result</TableCell>
-                        <TableCell>Points</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {results.map((result, index) => (
-                        <ResultsTableRow key={result.position} result={result} />
-                    ))}
-                </TableBody>
-            </Table>
-        </>
+      <RiseLoader
+        style={{
+          height: '50vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      />
     );
+  }
+
+  return (
+    <>
+      <h1>Results component</h1>
+      <Table>
+        <TableHead>
+          <TableRow className='table-header'>
+            <TableCell>Pos</TableCell>
+            <TableCell>Driver</TableCell>
+            <TableCell>Team</TableCell>
+            <TableCell>Result</TableCell>
+            <TableCell>Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {results.map((result, index) => (
+            <ResultsTableRow key={result.position} result={result} />
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 };
 
 export default RaceResults;
