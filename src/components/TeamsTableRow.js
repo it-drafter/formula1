667e3118124cab6ea23db5124758c1d3e7d578aa
portal222 <dvaportala@gmail.com>
2023-05-23 12:26:59 +0200

    Teams detalji

--- src/components/TeamsTableRow.js
+++ src/components/TeamsTableRow.js
@@ -1,17 +1,29 @@
-import React from "react";  
+import React from "react";
 import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';    
+import TableCell from '@mui/material/TableCell';
+import { useNavigate } from "react-router-dom";
+
 
 const TeamsTableRow = (props) => {
+  const navigate = useNavigate();
 
-return (
+  const handleClickConstructor = (constructorId) => {
+    console.log("Konstruktor dugme", constructorId);
+    const linkTo = `/teams${constructorId}`;
+    navigate(linkTo);
+
+
+  }
+
+  return (
     <TableRow>
-    <TableCell>{props.teams.position}</TableCell>
-    <TableCell>{props.teams.points}</TableCell>
-    <TableCell>{props.teams.wins}</TableCell>
-    <TableCell>{props.teams.Constructor.name}</TableCell>
-  </TableRow>
-)
+      <TableCell>{props.teams.position}</TableCell>
+      <TableCell>{props.teams.points}</TableCell>
+      <TableCell>{props.teams.wins}</TableCell>
+      <TableCell
+        onClick={() => handleClickConstructor(props.teams.Constructor.position)}>{props.teams.Constructor.name}</TableCell>
+    </TableRow>
+  )
 
 }
 export default TeamsTableRow;
