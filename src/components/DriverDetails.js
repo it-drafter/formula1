e13262e5ca10ac7bb810192ca691e9ee31bfe61e
goody86 <dobra.goody@gmail.com>
2023-05-24 13:07:50 +0200

    position

--- src/components/DriverDetails.js
+++ src/components/DriverDetails.js
@@ -72,7 +72,10 @@ const DriverDetails = () => {
                 {DetailRace.Results[0].Constructor.constructorId}
               </TableCell>
               <TableCell>{DetailRace.Results[0].grid}</TableCell>
-              <TableCell>{DetailRace.Results[0].position}</TableCell>
+              <TableCell className={"position_" + DetailRace.Results[0].position}>
+                {DetailRace.Results[0].position}
+             
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
