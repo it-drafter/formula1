5cf44c42298547f337eee790f37f9fe7b091cfcb
itancik <itancik@gmail.com>
2023-05-24 11:59:36 +0200

    fix: buxfix

--- src/components/GrandPrixDetails.js
+++ src/components/GrandPrixDetails.js
@@ -26,7 +26,7 @@ const GrandPrixDetails = () => {
   //   console.log('komentar', props);
 
   const getGrandPrix = async () => {
-    const url = `http://ergast.com/api/f1/2013/results/${round}.json`;
+    const url = `http://ergast.com/api/f1/2013/results/1.json`;
     setIsLoading(true);
     try {
       const response = await axios.get(url);
