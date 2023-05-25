53cd6c66b0e44ea592df7459f24f61c94d436caf
itancik <itancik@gmail.com>
2023-05-25 10:13:34 +0200

    split TeamDetails component

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -2,14 +2,9 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { RiseLoader } from 'react-spinners';
-import {
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-} from '@mui/material';
+
 import GlobalContext from '../context/global-context';
+import TeamDetailsRaces from './TeamDetailsRaces';
 
 const TeamDetails = (props) => {
   const globalCtx = useContext(GlobalContext);
@@ -72,49 +67,13 @@ const TeamDetails = (props) => {
           <p>Points: {teamDetails.points}</p>
           <p>
             History:{' '}
-            <a href={teamDetails.Constructor.url + "#History"} target='_blank'>
+            <a href={teamDetails.Constructor.url + '#History'} target='_blank'>
               ↗
             </a>
           </p>
         </div>
       </div>
-      <Table>
-        <TableHead>
-          <TableRow className='table-header'>
-            <TableCell>Round</TableCell>
-            <TableCell>Race Name</TableCell>
-            <TableCell>{teamResults[0].Results[0].Driver.familyName}</TableCell>
-            <TableCell>{teamResults[0].Results[1].Driver.familyName}</TableCell>
-            <TableCell>Points</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {teamResults.map((teamResult) => {
-            return (
-              <TableRow key={teamResult.round}>
-                <TableCell>{teamResult.round}</TableCell>
-                <TableCell onClick={() => handleDrivers(teamResult.round)}>
-                  {teamResult.raceName}
-                </TableCell>
-                <TableCell
-                  className={'position_' + teamResult.Results[0].position}
-                >
-                  {teamResult.Results[0].position}
-                </TableCell>
-                <TableCell
-                  className={'position_' + teamResult.Results[1].position}
-                >
-                  {teamResult.Results[1].position}
-                </TableCell>
-                <TableCell>
-                  {Number(teamResult.Results[0].points) +
-                    Number(teamResult.Results[1].points)}
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+      <TeamDetailsRaces teamResults={teamResults} />
     </>
   );
 };
