70d7ea3b3879aad468924cf6c86677bce3e935fb
goody86 <dobra.goody@gmail.com>
2023-05-29 14:18:44 +0200

    flag

--- src/components/DriverDetailsCollapsable.js
+++ src/components/DriverDetailsCollapsable.js
@@ -47,8 +47,7 @@ const DriverDetailsCollapsable = (props) => {
         size='small'
         aria-label='purchases'
         sx={{ margin: 0, marginBottom: 5 }}
-        className='tableContainer'
-      >
+        className='tableContainer'>
         <TableHead>
           <TableRow>
             <TableCell>
