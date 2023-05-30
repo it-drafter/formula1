6ccd538ecc1dc837517802bd072c43fe2f1adcbd
itancik <itancik@gmail.com>
2023-05-30 20:33:04 +0200

    fix: Search folder rename attempt 4

---
+++ src/components/Search/SearchResultsRacesTableRow.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+//import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
+// import GlobalContext from '../context/global-context';
+// import DriverDetailsCollapsable from './DriverDetailsCollapsable';
+
+const SearchResultsRacesTableRow = (props) => {
+  //   const globalCtx = useContext(GlobalContext);
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  // console.log('Props', props.driver.nationality);
+  // console.log('Props111', props);
+
+  //   const handleClickDetails = (driverId) => {
+  //     console.log(driverId);
+  //     const linkTo = `/drivers/details/${driverId}`;
+  //     navigate(linkTo);
+  //   };
+  //   console.log('SEARCH PROPS: ', props);
+  return (
+    <>
+      <TableRow>
+        <TableCell
+          component='th'
+          scope='row'
+          //   onClick={() => handleClickDetails(props.driver.driverId)}
+          //   className='rucica'
+        >
+          {props.race.date}
+        </TableCell>
+        <TableCell
+        //   onClick={() => handleClickDetails(props.driver.Driver.driverId)}
+        //   className='rucica'
+        >
+          <div className='flagName'>
+            {/* {globalCtx.flagFn(props.driver.Driver.nationality)} */}
+            <span> </span>
+            <span> </span>
+            {props.race.raceName}
+          </div>
+        </TableCell>
+        <TableCell>
+          <a href={props.race.url} target='_blank'>↗</a></TableCell>
+        <TableCell>
+          {props.race.Circuit.circuitName} ({props.race.Circuit.circuitId})
+        </TableCell>
+        <TableCell>{props.race.Circuit.Location.locality}</TableCell>
+        <TableCell>{`${props.race.Results[0].Driver.givenName} ${props.race.Results[0].Driver.familyName} / ${props.race.Results[0].Constructor.name}`}</TableCell>
+      </TableRow>
+
+      {/* <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
+          <Collapse in={open} timeout='auto' unmountOnExit>
+            <Box sx={{ margin: 0 }}>
+              <DriverDetailsCollapsable
+              driverId={props.driver.Driver.driverId}
+              />
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow> */}
+    </>
+  );
+};
+export default SearchResultsRacesTableRow;
