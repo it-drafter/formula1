4d1d51e7650b2c412717b876da411bf658ec6621
itancik <itancik@gmail.com>
2023-05-31 21:25:25 +0200

    chore: github pages deploy

--- src/components/UI/Navigation.js
+++ src/components/UI/Navigation.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   NavLink,
+  HashRouter,
 } from 'react-router-dom';
 import Drivers from '../Drivers/Drivers';
 import Races from '../Races/Races';
@@ -40,7 +41,8 @@ export default function Navigation() {
 
   return (
     <div className='NavContainer'>
-      <Router>
+      {/* <Router> */}
+      <HashRouter basename='/'>
         <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
           <Container>
             <Link to='/' className='nav-link-home'>
@@ -51,7 +53,7 @@ export default function Navigation() {
               <ul>
                 <li> Drivers</li>
                 <li>
-                  <img src="/img/helmet.svg" className='kaciga' />
+                  <img src='/img/helmet.svg' className='kaciga' />
                 </li>
               </ul>
             </NavLink>
@@ -60,7 +62,7 @@ export default function Navigation() {
               <ul>
                 <li>Teams</li>
                 <li>
-                  <img src="/img/bolid.svg" className='teams1' />
+                  <img src='/img/bolid.svg' className='teams1' />
                 </li>
               </ul>
             </NavLink>
@@ -69,7 +71,7 @@ export default function Navigation() {
               <ul>
                 <li>Races</li>
                 <li>
-                  <img src="/img/raceFlags.svg" className='races2' />
+                  <img src='/img/raceFlags.svg' className='races2' />
                 </li>
               </ul>
             </NavLink>
@@ -95,7 +97,8 @@ export default function Navigation() {
           <Route path='/teams/search' element={<SearchResultsTeams />} />
           <Route path='/races/search' element={<SearchResultsRaces />} />
         </Routes>
-      </Router>
+      </HashRouter>
+      {/* </Router> */}
     </div>
   );
 }
