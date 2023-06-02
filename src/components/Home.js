867c7fcd628f035ffaefedb7a2c69bdde3e0b8a3
itancik <itancik@gmail.com>
2023-06-02 11:40:22 +0200

    refactor: clean the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
-// import Breadcrumbs from '@mui/material/Breadcrumbs';
-// import Link from '@mui/material/Link';
-// import YearSelect from './YearSelect';
-import Drivers from './Drivers/Drivers';
 import BreadCrumbs from './UI/BreadCrumbs';
 import RiseLoaderSpinner from './UI/RiseLoaderSpinner';
 import {
-  colors,
   Table,
   TableBody,
   TableCell,
@@ -15,22 +10,12 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import GlobalContext from '../context/global-context';
-import DriversTableRow from './Drivers/DriversTableRow';
 import YearSelect from './UI/YearSelect';
 import Footer from './UI/Footer';
 import { useNavigate } from 'react-router';
-import Container from 'react-bootstrap/Container';
 import Stack from '@mui/material/Stack';
 
-// import { useContext } from 'react';
-// import GlobalContext from '../context/global-context';
-
 const Home = () => {
-  // const [reRender, setReRender] = useState(false);
-  // const handleReRender = () => {
-  //   console.log('rerender');
-  //   setReRender(!reRender);
-  // };
   const globalCtx = useContext(GlobalContext);
   const [drivers, setDrivers] = useState([]);
   const navigate = useNavigate();
@@ -41,33 +26,15 @@ const Home = () => {
     getDrivers();
   }, [globalCtx.chosenYear]);
 
-  // const handleDriverDetails= (driverId) => {
-  //   console.log("klik na drivera" );
-  //   const linkTo= `/drivers/details/${driverId}`;
-  //   navigate(linkTo)
-  // }
-
-  // const [selectYear, setSelecetYear] = useState(null);
-
-  // const handleSelectedYear = (year) => {
-  //   console.log('Home component:', year);
-  //   // return year;
-  // };
-  // onClick={()=>handleDriverDetails(driver.Driver.driverId)}
-
   const getDrivers = async () => {
-    // const urlDrivers = `https://raw.githubusercontent.com/nkezic/f1/main/AllDrivers`;
     const urlDrivers = `https://ergast.com/api/f1/${globalCtx.chosenYear}/driverStandings.json`;
     try {
       const response = await axios.get(urlDrivers);
-      console.warn('response', response);
       const data =
         response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-      console.log(data, ' data');
       setDrivers(data);
       setIsLoading(false);
     } catch (err) {
-      //   console.log(err);
       setIsLoading(false);
       setError(err);
     }
@@ -78,7 +45,6 @@ const Home = () => {
   }
 
   const handleDriverDetails = (driverId) => {
-    console.log('klik na drivera', driverId);
     const linkTo = `/drivers/details/${driverId}`;
     navigate(linkTo);
   };
@@ -86,17 +52,7 @@ const Home = () => {
   if (isLoading) {
     return (
       <>
-        <RiseLoaderSpinner
-        // style={{
-        //   height: '50vh',
-        //   display: 'flex',
-        //   justifyContent: 'center',
-        //   alignItems: 'center',
-        // }}
-        />
-        {/* <Skeleton animation='wave' height={50} width='90%' /> 
-       <Skeleton variant="rounded" animation='wave' height={600} style={{ width: '90%', alignItems: 'center' }} />
-       <Placeholder bg='danger' style={{ width: '90%', height: '500px' }} />  */}
+        <RiseLoaderSpinner />
       </>
     );
   }
@@ -122,7 +78,6 @@ const Home = () => {
           src='./img/retro_poster.jpg'
         />
 
-        {/* <Drivers home={true} /> */}
         <Table className='table-home col'>
           <TableHead>
             <TableRow className='table-header'>
